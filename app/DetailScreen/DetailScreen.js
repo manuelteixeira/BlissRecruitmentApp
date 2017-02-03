5bd561037479eff40d51d542ca09f6fd11564bef
Manuel Teixeira <teixeirasmanuel@gmail.com>
2017-02-03 23:39:23 +0000

    routes when selecting questions

--- app/DetailScreen/DetailScreen.js
+++ app/DetailScreen/DetailScreen.js
@@ -3,7 +3,7 @@
 angular.module('myApp.DetailScreen', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/DetailScreen', {
+  $routeProvider.when('/DetailScreen/:id', {
     templateUrl: 'DetailScreen/DetailScreen.html',
     controller: 'DetailScreenCtrl'
   })
@@ -14,16 +14,18 @@ angular.module('myApp.DetailScreen', ['ngRoute'])
   });
 }])
 
-.controller('DetailScreenCtrl', function($scope, $http) {
+.controller('DetailScreenCtrl', function($scope, $http, $routeParams) {
   this.shareQuestionFlag = false;
 
-  $http.get("https://private-anon-0540f979da-blissrecruitmentapi.apiary-mock.com/questions/1")
- .then(function (response) { apiQuestion = response.data; });
- this.question = apiQuestion;
+
+  $http.get("https://private-anon-0540f979da-blissrecruitmentapi.apiary-mock.com/questions/" + $routeParams.id)
+ .then(function (response) { apiQuestion = response.data });
+
+this.question = apiQuestion;
 
  //Vote choice (update question API)
  $scope.voteChoice = function() {
-   $http.put("https://private-anon-0540f979da-blissrecruitmentapi.apiary-mock.com/questions/1", apiQuestion)
+   $http.put("https://private-anon-0540f979da-blissrecruitmentapi.apiary-mock.com/questions/" + $routeParams.id, apiQuestion)
    .then(function (response) {$scope.message = response.data });
  }
 
@@ -38,11 +40,9 @@ angular.module('myApp.DetailScreen', ['ngRoute'])
 
    $http.post('https://private-anon-0540f979da-blissrecruitmentapi.apiary-mock.com/share?'
    + $scope.emailInput +
-   '&https://private-anon-0540f979da-blissrecruitmentapi.apiary-mock.com/questions/1', apiQuestion)
+   '&https://private-anon-0540f979da-blissrecruitmentapi.apiary-mock.com/questions/' + $routeParams.id, apiQuestion)
    .then(function (response) {$scope.message = response.data});
  }
 });
 
 var apiQuestion;
-
-//https://private-anon-0540f979da-blissrecruitmentapi.apiary-mock.com/share?teixeirasmanuel@gmail.com&https://private-anon-0540f979da-blissrecruitmentapi.apiary-mock.com/questions/1
